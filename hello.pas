595b69048c19cb24d794c8cab0e5fb02357d1016
FerencBlanc <kv.belashova@gmail.com>
2024-10-10 18:25:00 +0300

    task-04 (lab 10.10)

--- hello.pas
+++ hello.pas
@@ -1,3 +1,20 @@
-﻿begin
-print('Hello, word!')
+﻿//begin
+//print('Hello, word!')
+//end.
+function HypoPerimRightTriangle(a, b: real): (real, real);
+begin
+  assert(a > 0);
+  assert(b > 0);
+  var c := Sqrt(a * a + b * b);
+  var p := a + b + c;
+  Result := (c, p);
+end;
+
+begin
+  loop 3 do
+  begin
+    var a := readreal;
+    var b := readreal;
+    HypoPerimRightTriangle(a, b).Println;
+  end;
 end.
---
+++ task-04 (lab 10.10).pas
@@ -0,0 +1,31 @@
+﻿//Описать функцию HypoPerimRightTriangle(a, b), вычисляющую гипотенузу c=√a2+b2 и периметр P=a+b+c
+//прямоугольного треугольника по его катетам a и b (положительные вещественные числа).
+//В основной программе найти гипотенузу и периметр для 3 заданных пар a и b.
+function HypoPerimRightTriangle(a, b: real): (real, real);
+begin
+  assert(a > 0);
+  assert(b > 0);
+  var c := Sqrt(a * a + b * b);
+  var p := a + b + c;
+  Result := (c, p);
+end;
+
+begin
+  loop 3 do
+  begin
+    var a := readreal;
+    var b := readreal;
+    HypoPerimRightTriangle(a, b).Println;
+  end;
+end.
+{
+log
+3
+4
+(5,12)
+12
+11
+(16.2788205960997,39.2788205960997)
+3
+8
+(8.54400374531753,19.5440037453175)
